290bee86bac680e6d0606b0519ca41d5dadb5217
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-10 21:34:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
     if @sender.balance > @amount && @status == 'pending'
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = 'completed'
+      @status = 'complete'
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
