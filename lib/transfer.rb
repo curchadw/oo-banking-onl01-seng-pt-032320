2507ab4e6ac122fefdd9042d70ae04254e161932
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-10 21:34:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,18 +28,18 @@ class Transfer
   end
 
   def reverse_transfer
-    # if @status == 'complete'
-    #   @sender.balance += @amount
-    #   @receiver.balance -= @amount
-    #   @status ='reversed'
-    # else
-    #   nil
-    # end
-    if @status == "complete"
-      @sender.deposit( @amount )
-      @receiver.deposit( @amount * -1)
-      @status = "reversed"
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status ='reversed'
+    else
+      nil
     end
+    # if @status == "complete"
+    #   @sender.deposit( @amount )
+    #   @receiver.deposit( @amount * -1)
+    #   @status = "reversed"
+    # end
   end
 
 
