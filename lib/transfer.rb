0a10daf1232f81bf2362d313e92fe3928cd3611f
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-10 21:36:49 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,7 +9,33 @@ class Transfer
   end
 
   def valid?
-    @sender.valid? && @receiver.valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if @sender.balance > @amount && @status == 'pending' && @sender.valid? && @receiver.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = 'complete'
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status ='reversed'
+    else
+      nil
+    end
+
   end
 
 
